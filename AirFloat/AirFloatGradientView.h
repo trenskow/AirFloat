c9686493ca6de030510414513118fe1a31dc4e4b
Kristian Trenskow <trenskow@me.com>
2012-02-14 09:24:48 +0100

    New CAGradientLayer backed UIView

---
+++ AirFloat/AirFloatGradientView.h
@@ -0,0 +1,16 @@
+//
+//  UIGradientView.h
+//  AirFloat
+//
+//  Created by Kristian Trenskow on 2/14/12.
+//  Copyright (c) 2012 The Famous Software Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirFloatGradientView : UIView
+
+- (NSArray*)backgroundGradientColors;
+- (NSArray*)backgroundGradientColorLocations;
+
+@end
