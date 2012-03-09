afc311a225f7217e8f06c102024bf12d77dc178e
Kristian Trenskow <trenskow@me.com>
2012-03-09 11:19:38 +0100

    Added getCount, getNameAtIndex - internally the _setValue was added to make class able to mutate already existing values.

--- Airfloat/libairfloat/AirFloat/WebHeaders.h
+++ Airfloat/libairfloat/AirFloat/WebHeaders.h
@@ -27,20 +27,23 @@ public:
     const char* valueForName(const char* name);
     void addValue(const char* name, const char* format, ...);
     
+    uint32_t getCount();
+    const char* getNameAtIndex(uint32_t index);
+    
     uint32_t getTotalLength();
     uint32_t getContent(void* buffer, uint32_t size);
     
 private:
     
-    WebHeaders();
-    uint32_t _parseHeaders(char* buffer, uint32_t length);
-
     struct header {
         char* name;
         char* value;
-        uint32_t length;
     };
     
+    WebHeaders();
+    uint32_t _parseHeaders(char* buffer, uint32_t length);
+    void _setValue(header* header, const char* name, const char* value);
+
     header* _headers;
     uint32_t _headerCount;
     
