f3d53970e9cb6e17b22a2c0fcfed7a6bbc429d38
Kristian Trenskow <trenskow@me.com>
2012-03-09 11:28:50 +0100

    Added Objective-C categories.

---
+++ Airfloat/libairfloat/AirFloat/UIDevice+AirFloatAdditions.h
@@ -0,0 +1,9 @@
+//
+//  UIDevice+AirFloatAdditions.h
+//  AirFloat
+//
+//  Created by Kristian Trenskow on 2/20/12.
+//  Copyright (c) 2012 The Famous Software Company. All rights reserved.
+//
+
+#define UICurrentDevice [UIDevice currentDevice]
