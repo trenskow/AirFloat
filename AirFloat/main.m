263d0cd2c509f2f390af169075bdf760c0250daa
Kristian Trenskow <trenskow@me.com>
2012-03-09 11:38:31 +0100

    Minor changes.

--- AirFloat/main.m
+++ AirFloat/main.m
@@ -3,7 +3,7 @@
 //  AirFloat
 //
 //  Created by Kristian Trenskow on 7/13/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 The Famous Software Company. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
