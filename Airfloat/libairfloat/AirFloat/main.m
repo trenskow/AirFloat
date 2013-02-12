0d3e0c2b06ff7bc610e1c48a66fc025bfe66c2cd
Kristian Trenskow <trenskow@me.com>
2013-02-12 22:46:18 +0100

    Changed name.

---
+++ Airfloat/libairfloat/AirFloat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirFloat
+//
+//  Created by Kristian Trenskow on 7/13/11.
+//  Copyright 2011 The Famous Software Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+    
+}
